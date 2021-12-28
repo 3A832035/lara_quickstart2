511436469f72597c3f7133137b2e72146fe6428a
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-12-29 01:08:57 +0800

    修正錯誤

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\models\Task;
 
 class TaskController extends Controller
 {
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Policies\TaskPolicy;
+use App\Task;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
